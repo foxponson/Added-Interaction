37e8b2d9d44f82f718a5c92ddc205888285eca21
foxponson <mourningfowl@gmail.com>
2025-11-19 02:59:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Basic greeting alert w star symbol~
+alert("Welcome To Fox's Page✩");
+
+
+// Overlay in & out opacity animation, starry background img, atop gradient
+const overlay = document.querySelector('.bg-overlay-container');
+overlay.animate(
+  [
+    { opacity: 0 },
+    { opacity: .1 },
+    { opacity: 0 }
+  ],
+  {
+    duration: 6000,
+    iterations: Infinity,
+    easing: "ease-in-out"
+  }
+);
